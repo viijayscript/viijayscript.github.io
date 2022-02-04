aaca1c5e80755550d3cc3f3fae38e0c4ddc0ed3b
ViijayScript <33539465+viijayscript@users.noreply.github.com>
2022-02-04 23:29:03 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
+let cacheData = "appV1";
+this.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(cacheData).then((cache) => {
+            cache.addAll([
+                https://viijayscript.github.io/
+            ])
+        })
+    )
+})
+
 self.addEventListener('fetch', function(event) {});
