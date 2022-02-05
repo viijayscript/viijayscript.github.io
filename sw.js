03c7560895d4cbca08dc0b38662843fdb9f67a1b
ViijayScript <33539465+viijayscript@users.noreply.github.com>
2022-02-05 15:49:43 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,44 @@
-let cacheData = "appV1";
-this.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open(cacheData).then((cache) => {
-            cache.addAll([
-                '/',
-                '/index.html'
-                "https://www.viijayscript.com/"
-            ])
-        })
-    )
-})
+const CACHE_NAME = "version-1";
+const urlsToCache = [ 'index.html', 'offline.html' ];
 
-self.addEventListener('fetch', function(event) {});
+const self = this;
+
+// Install SW
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+
+                return cache.addAll(urlsToCache);
+            })
+    )
+});
+
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request) 
+                    .catch(() => caches.match('offline.html'))
+            })
+    )
+});
+
+// Activate the SW
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+            
+    )
+});
